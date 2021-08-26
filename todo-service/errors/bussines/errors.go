4cf6c93b7c1ac9ec4c4b63f5c6b49d3df49f9985
Eduardo Flores <efloresambrosio@gmail.com>
2021-08-26 00:19:41 -0500

    new: implememt Unwrap to allow use As

--- todo-service/errors/bussines/errors.go
+++ todo-service/errors/bussines/errors.go
@@ -22,8 +22,10 @@ func (e *BadFieldForEntity) Error() string {
 	return fmt.Sprintf("badfieldforentity code %v message %v id %v type %v base: %v", e.Code, e.Message, e.EntityID, e.EntityType, e.Err)
 }
 
-//func (e *BadFieldForEntity) Unwrap() error { return e.Err }
+// Unwrap to allow use As for assertion type
+func (e *BadFieldForEntity) Unwrap() error { return e.Err }
 
+// NewBadFieldForEntity allow creates a new BadFieldForEntity
 func NewBadFieldForEntity(baseError myerrors.BaseError, entityType string, entityID int) *BadFieldForEntity {
 	return &BadFieldForEntity{BaseError: baseError, EntityType: entityType, EntityID: entityID}
 }
