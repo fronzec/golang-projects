69bbd6c7c4abb58d3cc37beff104eaedcc7011d4
Eduardo Flores <efloresambrosio@gmail.com>
2021-09-07 01:02:50 -0500

    chg: move example errors assertion to right package

--- todo-service/errors/errors.go
+++ todo-service/errors/errors.go
@@ -1,6 +1,13 @@
 package myerrors
 
-import "strconv"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"todo-service/errors/app"
+	"todo-service/errors/bussines"
+)
 
 type BaseError struct {
 	Err     error
@@ -14,4 +21,39 @@ func NewBaseError(err error, message string, code int) *BaseError {
 
 func (e *BaseError) Error() string {
 	return strconv.Itoa(e.Code) + " " + e.Message + ": " + e.Err.Error()
+}
+
+func TestErrors() {
+	configErr := app.NewBadConfigErr(BaseError{
+		Message: "db connection missing for db A",
+		Code:    1,
+	})
+
+	configErr2 := app.NewBadConfigErr(BaseError{
+		Message: "db connection missing for db B",
+		Code:    1,
+	})
+	var badConfig *app.BadConfigErr
+	// As is type assertion
+	fmt.Printf("As = %v \n", errors.As(configErr, &badConfig))
+	// Is compares against value, we need to implement method is for our err
+	fmt.Printf("Is = %v \n", errors.Is(configErr, configErr2))
+	printErr(configErr)
+	printErr(configErr2)
+
+	badFieldErr := bussines.NewBadFieldForEntity(
+		BaseError{
+			Err:     configErr,
+			Message: "bad field value limitField",
+			Code:    1000,
+		}, "user",
+		12345,
+	)
+	printErr(badFieldErr)
+	fmt.Printf("%v\n", errors.Is(configErr, configErr2))
+	fmt.Printf("%v\n", errors.As(badFieldErr, &badConfig))
+}
+
+func printErr(theError error) {
+	fmt.Println(theError)
 }
--- todo-service/main.go
+++ todo-service/main.go
@@ -1,45 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"os"
 
-	"todo-service/errors"
-	"todo-service/errors/app"
-	"todo-service/errors/bussines"
+	"todo-service/config"
 )
 
 func main() {
-	configErr := app.NewBadConfigErr(myerrors.BaseError{
-		Message: "db connection missing for db A",
-		Code:    1,
-	})
-
-	configErr2 := app.NewBadConfigErr(myerrors.BaseError{
-		Message: "db connection missing for db B",
-		Code:    1,
-	})
-	var badConfig *app.BadConfigErr
-	// As is type assertion
-	fmt.Printf("As = %v \n", errors.As(configErr, &badConfig))
-	// Is compares against value, we need to implement method is for our err
-	fmt.Printf("Is = %v \n", errors.Is(configErr, configErr2))
-	printErr(configErr)
-	printErr(configErr2)
-
-	badFieldErr := bussines.NewBadFieldForEntity(
-		myerrors.BaseError{
-			Err:     configErr,
-			Message: "bad field value limitField",
-			Code:    1000,
-		}, "user",
-		12345,
-	)
-	printErr(badFieldErr)
-	fmt.Printf("%v\n", errors.Is(configErr, configErr2))
-	fmt.Printf("%v\n", errors.As(badFieldErr, &badConfig))
+	err := run()
+	if err != nil {
+		fmt.Printf("app cannot start, err:%v",err)
+		os.Exit(0)
+	}
 }
 
-func printErr(theError error) {
-	fmt.Println(theError)
+func run() error {
+	_, err := config.NewPropertiesConfigProvider()
+	if err != nil {
+		return err
+	}
+	return nil
 }
