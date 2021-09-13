c3bfade33e249cbc02e5367f7d650888dfc21789
Eduardo Flores <efloresambrosio@gmail.com>
2021-09-12 21:41:11 -0500

    new: properties file provider, basic support

--- todo-service/config/properties_provider.go
+++ todo-service/config/properties_provider.go
@@ -1,16 +1,33 @@
 package config
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type PropertiesConfigProvider struct {
-	configs map[string]interface{}
+	Configs
 }
 
-func NewPropertiesConfigProvider() (Provider,error) {
+func (provider *PropertiesConfigProvider) logConfigs() {
+	fmt.Printf("=================================================\n")
+	for key, value := range provider.configs {
+		fmt.Printf("config: <%v=%v>\n", key, value)
+	}
+	fmt.Printf("=================================================\n")
+}
+
+func NewPropertiesConfigProvider() (Provider, error) {
 	provider := PropertiesConfigProvider{}
 	err := provider.loadConfig()
+	provider.logConfigs()
 	if err != nil {
 		return nil, err
 	}
-	return &provider,err
+	return &provider, err
 }
 
 func (provider *PropertiesConfigProvider) GetString(key string) (string, error) {
@@ -30,6 +47,45 @@ func (provider *PropertiesConfigProvider) GetFloat(key string) (float64, error) 
 }
 
 func (provider *PropertiesConfigProvider) loadConfig() error {
-	panic("implement me")
-}
+	configs := map[string]interface{}{}
+	fileName, ok := os.LookupEnv("PROPERTIES_FILENAME")
+	if !ok {
+		fmt.Printf("cannot read PROPERTIES_FILENAME environment, default will be used\n")
+		fileName = "./config/example/app.properties"
+	}
+	fmt.Printf("reading properfies file:%v\n", fileName)
+	open, err2 := os.Open(fileName)
+	if err2 != nil {
+		message := fmt.Sprintf("cannot open properties file %v error:%v\n", fileName, err2)
+		fmt.Printf(message)
+		return errors.New(message)
+	}
+	defer func(open *os.File) {
+		err := open.Close()
+		if err != nil {
+			fmt.Printf("cannot close file:%v\n", open)
+		}
+	}(open)
 
+	scanner := bufio.NewScanner(open)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if equal := strings.Index(line, "="); equal > 0 {
+			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+				value := ""
+				if len(line) > equal {
+					value = strings.TrimSpace(line[equal+1:])
+				}
+				configs[key] = value
+			}
+		}
+	}
+
+	if err3 := scanner.Err(); err3 != nil {
+		fmt.Printf("something goes wrong with the scanner %v\n", err3)
+		return err3
+	}
+	provider.configs = configs
+	return nil
+}
--- todo-service/config/provider.go
+++ todo-service/config/provider.go
@@ -1,9 +1,14 @@
 package config
 
+type Configs struct {
+	configs map[string]interface{}
+}
+
 type Provider interface {
 	GetString(key string) (string, error)
 	GetInt(key string) (int, error)
 	GetInt64(key string) (int64, error)
-	GetFloat(key string) (float64,error)
+	GetFloat(key string) (float64, error)
 	loadConfig() error
+	logConfigs()
 }
