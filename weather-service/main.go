6c0e5c15583af44e6c89cf2febece6be9e103c10
Eduardo Flores <efloresambrosio@gmail.com>
2025-02-10 23:31:56 -0600

    Set up HTTP server with /ping handler
    Replaced placeholder code with an HTTP server using net/http. Added a new /ping endpoint responding with an empty JSON object, and organized logic into a separate handlers package for better modularity. Logging was introduced to track server startup and errors.

--- weather-service/main.go
+++ weather-service/main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"weather-service/handlers"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	mux := http.NewServeMux()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	// Register handler for /ping
+	mux.HandleFunc("/ping", handlers.PingHandler)
+
+	// Start the server
+	log.Println("Server listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("Error starting the server: %v", err)
 	}
 }
