d0e618db7e0862db24e23edbdf2aa0b4b5b490d6
Eduardo Flores <efloresambrosio@gmail.com>
2025-03-25 16:36:29 -0600

    feat: initialize CLI application with cobra framework
    - Add basic root command structure
    - Configure basic CLI information and help message
    - Set up error handling for command execution

--- tmdbcli/main.go
+++ tmdbcli/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "tmdbcli",
+	Short: "TMDB CLI is a command line interface for The Movie Database",
+	Long: `A command line interface application that allows you to interact 
+with The Movie Database (TMDB) API to search for movies, TV shows, and more.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to TMDB CLI!")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	Execute()
 }
